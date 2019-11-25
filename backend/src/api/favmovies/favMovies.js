f00619b1f842c04f60270d308f7e3a55d8ee680e
Antônio Junior <acsjunior1@gmail.com>
2019-11-25 00:12:07 -0300

    Movie Attributes REST update

--- backend/src/api/favmovies/favMovies.js
+++ backend/src/api/favmovies/favMovies.js
@@ -4,13 +4,13 @@ const mongoose = restful.mongoose
 const movieSchema = new mongoose.Schema({
     owner: { type: String, required: true },
     index: { type: Number, required: true },
-    comment: { type: String, required: false },
     title: { type: String, required: true },
-    year: { type: Number, required: true },
-    //genre: { type: Array, required: true },
-    country: { type: String, required: false },
+    year: { type: String, required: true },
     poster: { type: String, required: true },
     imdbid: { type: String, required: true },
+    genre: { type: String, required: true },
+    country: { type: String, required: true },
+    plot: { type: String, required: true },
 })
 
 module.exports = restful.model('movies', movieSchema)
