92e517f7a366176cbe05a01fc5498f5eaf0d7add
Antônio Junior <acsjunior1@gmail.com>
2020-01-12 16:13:59 -0300

    docker compose

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,22 +1,24 @@
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 3003;
+const heroku = process.env.HEROKU;
 
-const path = require('path');
-const bodyParser = require('body-parser')
-const express = require('express')
-const server = express()
-const allowCors = require('./cors')
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const server = express();
+const allowCors = require('./cors');
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(allowCors);
 
-server.use(express.static('/app/frontend/public'));
-server.use(express.static('/app/frontend/dist'));
+if (heroku) {
+  server.use(express.static('/app/frontend/public'));
+  server.use(express.static('/app/frontend/dist'));
 
-// send the user to index html page
-server.get('/', (req, res) => {
-  res.sendFile('index.html', { root: "../app/frontend/public"});
-});
+  server.get('/', (req, res) => {
+    res.sendFile('index.html', { root: '/app/frontend/public' });
+  });
+}
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}.`);
